8166e4742d12415f3bdbaf3ea70ce17ce2fad368
dipeshgiri <50439654+dipeshgiri@users.noreply.github.com>
2019-05-09 13:23:44 +0545

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pan",
+      "name": "pan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.016769972484020456,
+        "pitch": 0.029672181773428008,
+        "fov": 1.4035938092030337
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.2457390345160455,
+          "pitch": 0.08904235282243178,
+          "title": "Tv wall",
+          "text": "includes wall cabinet,blank wall for tv,and low cabinet"
+        },
+        {
+          "yaw": 2.980064730049701,
+          "pitch": -0.006284176567746158,
+          "title": "bar",
+          "text": "Min. 3 sitters"
+        },
+        {
+          "yaw": -1.08727104455126,
+          "pitch": -0.030218161737366245,
+          "title": "aquarium desk",
+          "text": "includes wall cabinet"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
